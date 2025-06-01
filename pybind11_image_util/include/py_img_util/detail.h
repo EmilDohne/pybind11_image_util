05d54cfb0f96584b72eee4d163961e09eeece7c6
EmilDohne <86836589+EmilDohne@users.noreply.github.com>
2025-06-01 20:24:34 +0200

    add license information to headers

--- pybind11_image_util/include/py_img_util/detail.h
+++ pybind11_image_util/include/py_img_util/detail.h
@@ -1,4 +1,8 @@
-﻿#pragma once
+﻿// Copyright Contributors to the pybind11_image_util project.
+// SPDX-License-Identifier: BSD-3-Clause
+// https://github.com/EmilDohne/pybind11_image_util
+
+#pragma once
 
 #include <format>
 #include <vector>
--- pybind11_image_util/include/py_img_util/image.h
+++ pybind11_image_util/include/py_img_util/image.h
@@ -1,3 +1,7 @@
+// Copyright Contributors to the pybind11_image_util project.
+// SPDX-License-Identifier: BSD-3-Clause
+// https://github.com/EmilDohne/pybind11_image_util
+
 #pragma once
 
 #include <vector>
